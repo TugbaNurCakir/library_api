9898e3459eb2bb569abf5fd73fdee1f78235489c
tugbanur <nurtugba2110@gmail.com>
2021-12-20 21:49:52 +0300

    added

--- library_api/src/main/java/com/example/demo/Initializer.java
+++ library_api/src/main/java/com/example/demo/Initializer.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.Model.Entity.Book;
 import com.example.demo.Repository.AuthorRepository;
 import com.example.demo.Repository.BookRepository;
 import com.example.demo.Repository.UserRepository;
@@ -16,6 +17,21 @@ public class Initializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+//        Author author = authorRepository.save(Author.builder()
+//                .name("test")
+//                .surname("test")
+//                .build());
+        for (int i = 0; i < 100; i++) {
+            bookRepository.save(Book.builder()
+                    .title("initialize book " + i)
+                    .authorId(1)
+                    .build());
+        }
 
+
+//        userRepository.save(User.builder()
+//                .username("deneme")
+//                .password("12345")
+//                .build());
     }
 }
